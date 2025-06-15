284567a9d216119b320bddce0358802b8326e34a
0xtakemichi <fjerez034@gmail.com>
2025-06-15 02:45:04 -0400

    Add user handler with basic CRUD operations

---
+++ user.handler.js
@@ -0,0 +1,16 @@
+const User = {
+    list: (req, res) => {
+        res.status(200).send('List of users');
+    },
+    create: (req, res) => {
+        res.status(201).send('User created successfully');
+    },
+    update: (req, res) => {
+        res.status(204).send('User updated successfully');
+    },
+    destroy: (req, res) => {
+        res.status(204).send('User deleted successfully');
+    },
+}
+
+module.exports = User;
