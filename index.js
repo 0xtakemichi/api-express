2982bd6f60e11da840a8e5274e707b1dc0d7d3b3
0xtakemichi <fjerez034@gmail.com>
2025-06-24 23:19:53 -0400

    Get methods

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // change user, cluster and password
-mongoose.connect('')
+mongoose.connect()
 
 const User = mongoose.model('User', {
     username: String,
@@ -8,9 +8,32 @@ const User = mongoose.model('User', {
 })
 
 const createUser = async () => {
-    const user = new User({ username: 'takemi', age: 25 })
+    const user = new User({ username: 'felipe', age: 24 })
     const savedUser = await user.save()
     console.log(savedUser)
 }
+// createUser()
 
-createUser()
+const getUsers = async () => {
+    const users = await User.find()
+    console.log(users)
+}
+// getUsers()
+
+const getUserById = async (id) => {
+    const user = await User.findById(id)
+    console.log(user)
+}
+// getUserById('6852d6acd6154214145b3d41')
+
+const getUserByUsername = async (username) => {
+    const user = await User.find({ username: username })
+    console.log(user)
+}
+// getUserByUsername('takemi')
+
+const getOneUser = async (username) => {
+    const user = await User.findOne({ username: username })
+    console.log(user)
+}
+// getOneUser('felipe')
