c5a3c7c6ff13b7263fc01d669e695fd52ac79872
0xtakemichi <fjerez034@gmail.com>
2025-06-25 20:32:55 -0400

    Update and delete methods

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // change user, cluster and password
-mongoose.connect()
+mongoose.connect('mongodb+srv://0xtakemichi:Refuse 3Hankie1 Traverse3 Cosigner Slab Voyage2 Latticed@cluster0.zhm2eyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 const User = mongoose.model('User', {
     username: String,
@@ -37,3 +37,47 @@ const getOneUser = async (username) => {
     console.log(user)
 }
 // getOneUser('felipe')
+
+// UPDATE
+
+const updateUser = async (id, username) => {
+    const user = await User.findById(id)
+    user.username = username
+    const updatedUser = await user.save()
+    console.log(updatedUser)
+}
+//updateUser('6852d6acd6154214145b3d41', 'alonso')
+
+const updateUserAge = async (id, age) => {
+    const user = await User.findById(id)
+    user.age = age
+    const updatedUser = await user.save()
+    console.log(updatedUser)
+}
+// updateUserAge('6852d6acd6154214145b3d41', 30)
+
+// DELETE
+
+const deleteUser = async (id) => {
+    const user = await User.findByIdAndDelete(id)
+    console.log(user)
+}
+// deleteUser('6852d6acd6154214145b3d41')
+
+const deleteAllUsers = async () => {
+    const users = await User.deleteMany()
+    console.log(users)
+}
+// deleteAllUsers()
+
+const deleteOneUser = async (username) => {
+    const user = await User.findOne({ username: username })
+    console.log(user)
+    if (user) {
+        await user.remove()
+        console.log('User deleted')
+    } else {
+        console.log('User not found')
+    }
+}
+// deleteOneUser('felipe')
